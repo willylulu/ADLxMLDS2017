edfdb745c9aee1ba89359fca6007f95fdb84ef7f
willy3364 <maya6282@gmail.com>
2017-10-08 01:51:16 +0800

    Add test.rb to test git function

--- hw1/pretrain.py
+++ hw1/pretrain.py
@@ -1,16 +1,16 @@
 import os
 import numpy as np
 
-mfcc_train_rawData = open("../../data/mfcc/train.ark","r").readlines()
+mfcc_train_rawData = open("../../data/mfcc/train.ark","r").read().splitlines()
 #mfcc_test_rawData = open("../../data/mfcc/test.ark","r").readlines()
-train_label_rawData = open("../../data/train.lab","r").readlines()
+train_label_rawData = open("../../data/train.lab","r").read().splitlines()
 print(len(mfcc_train_rawData))
 # print(len(mfcc_test_rawData))
 print(len(train_label_rawData))
 
 mfcc_train_data = np.zeros((len(mfcc_train_rawData),39))
 mfcc_train_data_index = {}
-train_label = np.chararray(len(mfcc_train_rawData))
+train_label = np.zeros(len(mfcc_train_rawData))
 
 # mfcc_test_data = []
 # mfcc_test_data_index = {}
@@ -32,10 +32,16 @@ print(len(mfcc_train_data[0]))
 
 # print(len(mfcc_test_data[0]))
 
+phone_char48 = open("../../data/48phone_char.map").read().splitlines()
+letMap = {}
+for x in phone_char48:
+    tempX = x.split('\t')
+    letMap[tempX[0]] = int(tempX[1])
+
 for i in range(0,len(train_label_rawData)):
     tempStr = train_label_rawData[i]
     tempX = tempStr.split(',')
-    train_label[mfcc_train_data_index[tempX[0]]] = tempX[1]
+    train_label[mfcc_train_data_index[tempX[0]]] = int(letMap[tempX[1]])
 
 mfcc_train_data.dump("../../data/mfcc_x.dat")
 train_label.dump("../../data/y.dat")
--- hw1/train.py
+++ hw1/train.py
@@ -1,4 +1,38 @@
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, RNN, Conv1D
-from keras.optimizers
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import recurrent
+import keras.optimizers
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+X_train = np.load("../../data/mfcc_x.dat")
+Y_train = np.load("../../data/y.dat")
+
+print(Y_train)
+
+x_train, x_test, y_train, y_test = train_test_split(X_train, Y_train, train_size=0.8)
+
+print(x_train.shape)
+print(y_train.shape)
+
+y_train = keras.utils.to_categorical(y_train, num_classes=48)
+y_test = keras.utils.to_categorical(y_test, num_classes=48)
+
+model = Sequential()
+model.add(Dense(64, input_dim=39, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(48, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          epochs=20,
+          batch_size=128,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, batch_size=8192)
+print(score)
